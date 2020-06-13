b6d6e5b60507f509991a70dff978f049739ef8d1
mehmet satilmis <mehmetsatilmisgyte@gmail.com>
2020-06-13 15:31:10 +0300

    Post .gitignore changes

--- course-04/exercises/lesson-2/client/src/config.ts
+++ course-04/exercises/lesson-2/client/src/config.ts
@@ -1,1 +1,1 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://k95i1l9msj.execute-api.us-east-2.amazonaws.com/dev'
--- course-04/exercises/lesson-4/client/src/config.ts
+++ course-04/exercises/lesson-4/client/src/config.ts
@@ -1,1 +1,2 @@
-export const apiEndpoint = '...'
+const apiId = '4z3m5c210k'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
